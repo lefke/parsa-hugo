fee684e57b29a9add31744245f36dd8035648aeb
lefke <ailurus@gmail.com>
2020-10-24 21:22:31 +0100

    add imagesLoaded console logging

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,18 +45,43 @@
     ]
   });
 
-
+  // Masonry
+  setTimeout(function(){
+    $('.masonry-container').masonry({
+      itemSelector: '.masonry-container > div',
+      columnWidth: 1
+    });
+  }, 500);
   
-  // init Masonry
-  var $grid = $('.masonry-container').masonry({
-    itemSelector: '.masonry-container > div',
-    columnWidth: 1
-  });
-  // layout Masonry after each image loads
-  $grid.imagesLoaded().progress( function() {
-    $grid.masonry('layout');
+  $('.masonry-container').imagesLoaded( function() {
+    // images have loaded
   });
 
+  // options
+  $('.masonry-container').imagesLoaded( {
+    // options...
+    },
+    function() {
+      // images have loaded
+    }
+  );
+  
+  $('.masonry-container').imagesLoaded()
+    .always( function( instance ) {
+      console.log('all images loaded');
+    })
+    .done( function( instance ) {
+      console.log('all images successfully loaded');
+    })
+    .fail( function() {
+      console.log('all images loaded, at least one is broken');
+    })
+    .progress( function( instance, image ) {
+      var result = image.isLoaded ? 'loaded' : 'broken';
+      console.log( 'image is ' + result + ' for ' + image.img.src );
+    });
+    
+    
   // instafeed
   if (($('#instafeed').length) !== 0) {
     var userId = $('#instafeed').attr('data-userId');
